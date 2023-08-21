ae7cbba2e0462dc0ca1803feb716d58875e431b7
Marnin-A <msmaudu@gmail.com>
2023-08-21 15:54:51 +0100

    Added error handling to api request

--- Project8/script.js
+++ Project8/script.js
@@ -1,37 +1,25 @@
 const section = document.getElementById("main");
-const query = document.getElementById("search_box");
-// const
+const input = document.getElementById("search_box");
 
-document.getElementById("btn").addEventListener("click", async () => {
-  const accessKey = "DQGvYlaiaTMdASe6-kYW17XFLeIuTVaHvKKNFgS1zZM";
-  // const url0 = `https://api.unsplash.com/search/photos?query=${query}&client_id=${accessKey}&per_page=20`;
-  const url1 = `https://api.unsplash.com/search/photos?client_id=${accessKey}&page=1&query=${query.value}`;
-  const response = await fetch(url1);
-  const data = await response.json();
-  console.log(data.results);
-  console.log(query.value);
-  const values = data.results;
+document.getElementById("btn").addEventListener("click", async function () {
+  try {
+    const accessKey = "DQGvYlaiaTMdASe6-kYW17XFLeIuTVaHvKKNFgS1zZM";
+    const endpoint = `https://api.unsplash.com/search/photos?client_id=${accessKey}&page=1&query=${input.value}`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const values = data.results;
+    console.log(input.value);
+    console.log(data);
 
-  for (let i = 0; i < values.length; i++) {
-    console.log("Man");
-    const newImage = document.createElement("img");
-    newImage.src = values[i].urls.small;
-    console.log(values[i].urls.small);
-    section.append(newImage);
-    console.log(section.childNodes);
+    if (data.results !== undefined) {
+      for (let i = 0; i < values.length; i++) {
+        console.log("Ok");
+        const newImage = document.createElement("img");
+        newImage.src = values[i].urls.small_s3;
+        section.appendChild(newImage);
+      }
+    }
+  } catch (error) {
+    console.error(error);
   }
-  // data.forEach((element) => {
-  //   console.log("Man");
-  //   const newImage = document.createElement("img");
-  //   newImage.src = element.urls.small;
-  //   console.log(element.urls.small);
-  //   section.append(newImage);
-  //   console.log(section.childNodes);
-  // });
-
-  // console.log(section.childNodes[0]);
-  // for (let index = 0; index < data.length; index++) {
-  //   section.childNodes[index].src = response[index].urls.small;
-  // }
-  // image.src = data[0].urls.raw;
 });
